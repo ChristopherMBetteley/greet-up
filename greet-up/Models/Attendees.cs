4bb09d2a48634f0364ba6ed7160249c53c82c808
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-10-06 10:15:57 -0400

    first commit

---
+++ greet-up/Models/Attendees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace greet_up.Models
+{
+    public class Attendees
+    {
+        public int AttendeesID { get; set; }
+        public string AttendeesName { get; set; }
+        public string AttendeesEmail { get; set; }
+        public virtual ICollection<Events> Events { get; set; }
+    }
+}
---
+++ greet-up/Models/Events.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace greet_up.Models
+{
+    public class Events
+    {
+        [Key]
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventLocation { get; set; }
+        public string EventDescription { get; set; }
+
+        [ForeignKey("Attendees")]
+        public int AttendeesID { get; set; }
+        public string AttendeeName { get; set; }
+        public string AttendeeEmail { get; set; }
+    }
+}
